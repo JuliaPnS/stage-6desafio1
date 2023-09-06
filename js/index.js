fd9865388253dbd3804186752cd6677984f155d3
“JuliaPnS” <ju.pennas@gmail.com>
2023-09-05 22:58:21 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//iniciar o javascript
+//mapeamento das rotas
+//Essa const "virou" um objeto, dentro dela construímos vários objetos
+
+import { Router } from './router.js'
+
+const router = new Router()
+router.add('/', '/pages/home.html')
+router.add('/about', '/pages/about.html')
+router.add('/contact', '/pages/contact.html')
+router.add(404, '/pages/404.html')
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
